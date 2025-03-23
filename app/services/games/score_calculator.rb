079c2067388f38fd9af11dff4b256b7d46a01922
urugus <suguru.sakashita@gmail.com>
2025-03-23 18:06:43 +0900

    スコア計算を単純にターン数のみにする

--- app/services/games/score_calculator.rb
+++ app/services/games/score_calculator.rb
@@ -1,15 +1,12 @@
 module Games
   class ScoreCalculator
-    # スコア計算のパラメータ
-    BASE_SCORE_PER_TURN = 100
-
     # スコアを計算する
     # @param turn_count [Integer] ターン数
     # @param duration_seconds [Integer] ゲーム時間（秒）
     # @return [Hash] スコア情報
     def calculate(turn_count, duration_seconds)
-      # スコア = ターン数 * BASE_SCORE_PER_TURN
-      score = turn_count * BASE_SCORE_PER_TURN
+      # スコア = ターン数（単純にターン数のみをスコアとする）
+      score = turn_count
 
       {
         score: score,
