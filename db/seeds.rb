7177e6bd85b1055bf756c00537b3fbceaeb7f3b7
urugus <suguru.sakashita@gmail.com>
2025-03-23 16:20:14 +0900

    初期データ投入をrake db:seedで実行できるように修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,16 @@
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+
+puts "=== シードデータの準備を開始します ==="
+
+# Rubyキーワードのインポート
+Rake::Task["words:import_keywords"].invoke
+
+# Rubyドキュメントから単語をインポート
+Rake::Task["words:import_from_doctree"].invoke
+
+# 単語の説明を更新
+Rake::Task["words:update_descriptions"].invoke
+
+puts "=== シードデータの準備が完了しました ==="
