e0f8c38a724797b2a89e91a9478127a14100ff3a
urugus <suguru.sakashita@gmail.com>
2025-04-13 22:07:10 +0900

    fix: session_manager_specのインデントと不足していたendを修正

--- spec/services/games/session_manager_spec.rb
+++ spec/services/games/session_manager_spec.rb
@@ -123,10 +123,11 @@ RSpec.describe Games::SessionManager do
     context '応答可能な単語がある場合' do
       it '単語を選択し、プレイヤーのターンに切り替える' do
         result = session_manager.computer_turn
-expect(result[:valid]).to be true
-expect(result[:word]).to be_present
-expect(result[:word_object]).to be_a(Word)
-expect(session_manager.current_state).to eq(Games::SessionManager::GAME_STATE[:player_turn])
+        expect(result[:valid]).to be true
+        expect(result[:word]).to be_present
+        expect(result[:word_object]).to be_a(Word)
+        expect(session_manager.current_state).to eq(Games::SessionManager::GAME_STATE[:player_turn])
+      end
     end
 
     context '前の単語が記号で終わる場合' do
