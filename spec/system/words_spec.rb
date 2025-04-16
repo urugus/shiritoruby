4edfdbd2b247701168ed6290a5d168e0d8657ac5
urugus <suguru.sakashita@gmail.com>
2025-04-16 15:55:35 +0900

    単語管理ページに検索機能と削除機能を追加
    - 単語または説明文で検索できる機能を追加
    - 検索結果をクリアできる機能を追加
    - 一覧から1単語を削除できる機能を追加
    - テストを追加

--- spec/system/words_spec.rb
+++ spec/system/words_spec.rb
@@ -73,4 +73,74 @@ RSpec.describe "Words", type: :system do
       expect(page).to have_content("System test keyword")
     end
   end
+
+  describe "検索機能", js: false do
+    before do
+      # テスト用の単語を作成
+      @word1 = create(:word, word: "search_word", description: "This is a searchable word")
+      @word2 = create(:word, word: "another_word", description: "This is another word")
+      @word3 = create(:word, word: "third_word", description: "Contains search term")
+
+      visit words_path
+    end
+
+    it "単語を検索できる" do
+      # 検索フォームに入力
+      fill_in "query", with: "search"
+      click_button "検索"
+
+      # 検索結果を確認
+      expect(page).to have_content(@word1.word)
+      expect(page).to have_content(@word3.description)
+      expect(page).not_to have_content(@word2.word)
+
+      # 検索クエリがフォームに残っていることを確認
+      expect(find_field("query").value).to eq("search")
+
+      # クリアボタンが表示されていることを確認
+      expect(page).to have_link("クリア")
+    end
+
+    it "検索結果をクリアできる" do
+      # 検索を実行
+      fill_in "query", with: "search"
+      click_button "検索"
+
+      # クリアボタンをクリック
+      click_link "クリア"
+
+      # すべての単語が表示されていることを確認
+      expect(page).to have_content(@word1.word)
+      expect(page).to have_content(@word2.word)
+      expect(page).to have_content(@word3.word)
+
+      # 検索フォームがクリアされていることを確認（値の検証ではなく、フィールドの存在を確認）
+      expect(page).to have_field("query")
+      # クエリパラメータがないことを確認
+      expect(current_url).not_to include("query=")
+    end
+  end
+
+  describe "削除機能", js: false do
+    before do
+      @word = create(:word, word: "delete_test", description: "This word will be deleted")
+      visit words_path
+    end
+
+    it "単語を削除できる" do
+      # 削除前の単語数を記録
+      initial_count = Word.count
+
+      # 削除リンクのパスを直接取得して削除リクエストを送信
+      delete_path = word_path(@word)
+      page.driver.submit :delete, delete_path, {}
+
+      # 削除後のページを表示
+      visit words_path
+
+      # 単語が削除されたことを確認
+      expect(Word.count).to eq(initial_count - 1)
+      expect(page).not_to have_content(@word.word)
+    end
+  end
 end
